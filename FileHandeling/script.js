3f71a0c38d0e738c3e4308b8a3145fc7ca9a6aa9
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2024-01-09 09:08:05 +0530

    Add files via upload

---
+++ FileHandeling/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.getElementById('fileInput');
+    const editor = document.getElementById('editor');
+
+    fileInput.addEventListener('change', handleFileSelect);
+});
+
+function handleFileSelect(event) {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const content = e.target.result;
+            document.getElementById('editor').value = content;
+        };
+
+        reader.readAsText(file);
+    }
+}
+
+function saveFile() {
+    const content = document.getElementById('editor').value;
+    const blob = new Blob([content], { type: 'text/plain' });
+    
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'edited_file.txt';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
