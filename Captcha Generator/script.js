200d50770072536a392349d8b13530000d58ead3
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2024-01-26 18:01:21 +0530

    Update script.js

--- Captcha Generator/script.js
+++ Captcha Generator/script.js
@@ -2,6 +2,7 @@ let submitButton = document.getElementById("submit-button");
 let userInput = document.getElementById("user-input");
 let canvas = document.getElementById("canvas");
 
+
 let reloadButton = document.getElementById("reload-button");
 let text = "";
 const textGenerator = () => {
